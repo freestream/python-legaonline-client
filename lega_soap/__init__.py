1211c3a473a8f728b4f5af5f80c28201d04e59c0
Anton Samuelsson <samuelsson.anton@gmail.com>
2026-08-31 17:12:21 +0200

    Bump version to 1.0.3
    Sync __version__ in __init__.py (was stale at 1.0.0) with pyproject and
    release the OccasionStatus enum.

--- lega_soap/__init__.py
+++ lega_soap/__init__.py
@@ -2,7 +2,7 @@ from .client import Client
 from .financial_client import FinancialClient
 from .enums import OccasionStatus
 
-__version__ = "1.0.0"
+__version__ = "1.0.3"
 __all__ = [
     "Client",
     "FinancialClient",
